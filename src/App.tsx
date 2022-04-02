918209d690b1997304272b5921aab68ff16fa931
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-03 01:34:27 +0800

    feat: setting router path

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,37 @@ import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 
 import Header from "./components/Header";
 import SimpleSidebar from "./components/Siderbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Outlet, Routes } from "react-router-dom";
 import Article from "./pages/Article";
 import Category from "./pages/Category";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Account from "./pages/Account";
 
 export const App = () => (
   <BrowserRouter>
     <ChakraProvider theme={theme}>
       <Routes>
-        <Header />
-        <Box textAlign="center" fontSize="xl">
-          <SimpleSidebar>
-            <Route path="/article" element={<Article />} />
-            <Route path="/category" element={<Category />} />
-            <Route path="/login" element={<Login />} />
-          </SimpleSidebar>
-        </Box>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Header />
+              <Box textAlign="center" fontSize="xl">
+                <SimpleSidebar>
+                  <Outlet />
+                </SimpleSidebar>
+              </Box>
+            </div>
+          }
+        >
+          <Route path="/article" element={<Article />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/account" element={<Account />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </ChakraProvider>
   </BrowserRouter>
