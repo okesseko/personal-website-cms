9bfb2391efb69d0d1f77f3cdf0364a049e0a1ab7
okesseko <59650459+okesseko@users.noreply.github.com>
2022-04-04 20:50:12 +0800

    feat: redirect root path to article

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 
 import Header from "./components/Header";
 import SimpleSidebar from "./components/Siderbar";
-import { BrowserRouter, Route, Outlet, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Outlet, Routes, Navigate } from "react-router-dom";
 import Article from "./pages/Article";
 import Category from "./pages/Category";
 import Login from "./pages/Login";
@@ -28,6 +28,7 @@ export const App = () => (
             </div>
           }
         >
+          <Route path="" element={<Navigate to="/article" replace />} />
           <Route path="/article" element={<Article />} />
           <Route path="/category" element={<Category />} />
           <Route path="/account" element={<Account />} />
