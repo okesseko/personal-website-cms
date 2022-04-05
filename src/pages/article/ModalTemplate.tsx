f55629aeaacfe284e530b49f0b2f92c21e11b70a
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-06 00:47:18 +0800

    feat: add upload prev image & add margin in each fields

--- src/pages/article/ModalTemplate.tsx
+++ src/pages/article/ModalTemplate.tsx
@@ -1,34 +1,53 @@
 import {
+  Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Icon,
   Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Textarea,
   useColorModeValue,
 } from "@chakra-ui/react"
 import React from "react"
 import { useForm } from "react-hook-form"
 import FileUpload from "../../components/FileUpload"
 import Modal from "../../components/Modal"
-
+import { FiFile } from "react-icons/fi"
+import convertImageToBase64 from "../../utils/convertImageToBase64"
+import { postArticle } from "../../api"
 interface ModalTemplateProps {
   isOpen: boolean
   onClose: () => void
+  categoryList: { name: string; value: string | number }[]
+  statusList: { name: string; value: string | number }[]
 }
 
-const ModalTemplate = ({ isOpen, onClose }: ModalTemplateProps) => {
+const ModalTemplate = ({
+  isOpen,
+  onClose,
+  categoryList,
+  statusList,
+}: ModalTemplateProps) => {
   const {
     handleSubmit,
     register,
     reset,
-    control,
+    resetField,
+    watch,
     formState: { errors },
   } = useForm()
+  const watchPreviewImg = watch("previewImg", [])
 
   const fields: Omit<FormFieldProps, "errors">[] = [
     {
       title: "Title",
       value: "title",
-      isRequired: true,
       type: "text",
       placeholder: "Input title",
       detail: register("title", { required: "Please input title" }),
@@ -36,42 +55,30 @@ const ModalTemplate = ({ isOpen, onClose }: ModalTemplateProps) => {
     {
       title: "Category",
       value: "category",
-      isRequired: true,
-      type: "text",
+      type: "select",
       placeholder: "Select category",
       detail: register("category", { required: "Please select category" }),
-    },
-    {
-      title: "Preview Image",
-      value: "previewImg",
-      isRequired: true,
-      type: "image",
-      placeholder: "Upload image",
-      detail: register("Image", { required: "Please upload image" }),
+      options: categoryList,
     },
     {
       title: "Content",
       value: "content",
-      isRequired: true,
-      type: "text",
+      type: "textarea",
       placeholder: "Input content",
       detail: register("content", { required: "Please input content" }),
     },
     {
       title: "Emotion Icon",
       value: "emotionIcon",
-      isRequired: true,
       type: "text",
       placeholder: "Input emotion icon",
       detail: register("emotionIcon", {
         required: "Please input emotionIcon",
-        maxLength: 3,
       }),
     },
     {
       title: "Emotion Number",
       value: "emotionNumber",
-      isRequired: true,
       type: "number",
       placeholder: "Input emotion number",
       detail: register("emotionNumber", {
@@ -81,15 +88,14 @@ const ModalTemplate = ({ isOpen, onClose }: ModalTemplateProps) => {
     {
       title: "Status",
       value: "status",
-      isRequired: true,
-      type: "number",
+      type: "select",
       placeholder: "Select status",
       detail: register("status", { required: "Please select status" }),
+      options: statusList,
     },
     {
       title: "Release Time",
       value: "releaseTime",
-      isRequired: true,
       type: "date",
       placeholder: "Select release time",
       detail: register("releaseTime", {
@@ -98,26 +104,49 @@ const ModalTemplate = ({ isOpen, onClose }: ModalTemplateProps) => {
     },
   ]
 
+  const validateFiles = (value: FileList) => {
+    if (value.length < 1) {
+      return "Files is required"
+    }
+    for (const file of Array.from(value)) {
+      const fsMb = file.size / (1024 * 1024)
+      const MAX_FILE_SIZE = 10
+      if (fsMb > MAX_FILE_SIZE) {
+        return "Max file size 10mb"
+      }
+    }
+    return true
+  }
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
-      onSubmit={handleSubmit(val => console.log(val, errors))}
+      onSubmit={handleSubmit(async val => {
+        const base64Image = await convertImageToBase64(val.previewImg[0]).then(
+          data => data
+        )
+        postArticle({
+          ...val,
+          previewImg: base64Image,
+        })
+        console.log({
+          ...val,
+          previewImg: base64Image,
+        })
+        onClose()
+      })}
       onReset={() => reset({})}
     >
       <FileUpload
-        name="avatar"
-        acceptedFileTypes="image/*"
-        isRequired={true}
-        placeholder="Your avatar"
         errors={errors}
-        control={control}
-      >
-        New avatar
-      </FileUpload>
-      {/* {fields.map(field => (
+        register={register("previewImg", { validate: validateFiles })}
+        uploadFile={watchPreviewImg ? watchPreviewImg[0] : undefined}
+        handleClear={() => resetField("previewImg")}
+      />
+      {fields.map(field => (
         <FormField key={field.value} errors={errors} {...field} />
-      ))} */}
+      ))}
     </Modal>
   )
 }
@@ -126,32 +155,84 @@ interface FormFieldProps {
   errors: { [x: string]: any }
   title: string
   value: string
-  isRequired: boolean
   detail: any
-  type: React.HTMLInputTypeAttribute
+  type: "text" | "number" | "date" | "select" | "textarea"
   placeholder: string
+  options?: { name: string; value: string | number }[]
 }
 
 const FormField = ({
   errors,
   title,
   value,
-  isRequired,
   detail,
   type,
   placeholder,
+  options = [],
 }: FormFieldProps) => {
+  const borderColor = useColorModeValue("gray.700", "gray.200")
+
+  function Field() {
+    switch (type) {
+      case "text":
+      case "date":
+        return (
+          <Input
+            type={type}
+            placeholder={placeholder}
+            borderColor={borderColor}
+            {...detail}
+          />
+        )
+      case "textarea":
+        return (
+          <Textarea
+            size="md"
+            resize="vertical"
+            placeholder={placeholder}
+            borderColor={borderColor}
+            {...detail}
+          />
+        )
+      case "number":
+        return (
+          <NumberInput
+            min={1}
+            max={20}
+            placeholder={placeholder}
+            borderColor={borderColor}
+            {...detail}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        )
+      case "select":
+        return (
+          <Select
+            placeholder={placeholder}
+            borderColor={borderColor}
+            {...detail}
+          >
+            {options.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.name}
+              </option>
+            ))}
+          </Select>
+        )
+    }
+  }
+
   return (
     <FormControl /*isRequired={isRequired}*/ isInvalid={errors[value]}>
       <FormLabel fontSize="14px" htmlFor={value}>
         {title}
       </FormLabel>
-      <Input
-        type={type}
-        placeholder={placeholder}
-        borderColor={useColorModeValue("gray.700", "gray.200")}
-        {...detail}
-      />
+      <Field />
       <FormErrorMessage>
         {errors[value] && errors[value].message}
       </FormErrorMessage>
