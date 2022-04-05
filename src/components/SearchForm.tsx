114b5b30ddcd3af38623f73889acbf4a2c0f8872
okesseko <59650459+okesseko@users.noreply.github.com>
2022-04-05 14:34:01 +0800

    feat: add search box

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,80 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  Select,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react"
+
+import React from "react"
+import { useForm } from "react-hook-form"
+
+interface FieldProps {
+  title: string
+  value: string
+  type: "text" | "number" | "select"
+  placeholder: string
+  options?: { name: string; value: string }[]
+}
+
+interface SearchFormProps {
+  fields: FieldProps[]
+  onSubmit: (val: any) => void
+}
+
+const SearchForm = ({ fields, onSubmit }: SearchFormProps) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm()
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack direction={["column", "row"]} spacing="16px" textAlign="start">
+        {fields.map(({ title, value, type, placeholder, options = [] }) => (
+          <FormControl width="33%">
+            <FormLabel fontSize="14px" htmlFor={value}>
+              {title}
+            </FormLabel>
+            {type === "select" ? (
+              <Select
+                borderColor={useColorModeValue("gray.700", "gray.200")}
+                {...register(value)}
+              >
+                {options.map(option => (
+                  <option value={option.value}>{option.name}</option>
+                ))}
+              </Select>
+            ) : (
+              <Input
+                id="first-name"
+                type={type}
+                placeholder={placeholder}
+                borderColor={useColorModeValue("gray.700", "gray.200")}
+                {...register(value)}
+              />
+            )}
+          </FormControl>
+        ))}
+      </Stack>
+      <HStack margin="10px 0" justify="flex-end">
+        <Button type="submit" disabled={isSubmitting} colorScheme="blue">
+          Search
+        </Button>
+        <Button type="reset" colorScheme="blue" variant="outline">
+          Clear
+        </Button>
+      </HStack>
+    </form>
+  )
+}
+
+export default SearchForm
+export type { FieldProps }
