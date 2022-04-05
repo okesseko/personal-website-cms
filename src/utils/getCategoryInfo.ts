8aaf18c4ecadb064616c89baafed2fc24bd492ff
okesseko <59650459+okesseko@users.noreply.github.com>
2022-04-05 18:05:33 +0800

    feat: add getCategoryInfo util

---
+++ src/utils/getCategoryInfo.ts
@@ -0,0 +1,8 @@
+
+
+
+const getCategoryInfo = (categoryList: any, targetValue: string) => {
+  return categoryList.find((category:any) => category.value === targetValue)
+}
+
+export default getCategoryInfo
