8b98fbcd03ec27fecf68ac0ad1f3cfc7d0e86e49
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-11 22:37:19 +0800

    feat: category page

---
+++ src/pages/Category/Category.tsx
@@ -0,0 +1,168 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { getCategory, deleteCategory } from "../../api"
+import Modal from "../../components/Modal"
+import Pagination from "../../components/Pagination"
+import SearchForm, { FieldProps } from "../../components/SearchForm"
+import Table, { TableDataProps, TableHeaderProps } from "../../components/Table"
+import { MdAdd } from "react-icons/md"
+import { FiAlertCircle } from "react-icons/fi"
+import ModalTemplate from "./ModalTemplate"
+
+const TABLE_HEADER: TableHeaderProps[] = [
+  {
+    type: "string",
+    title: "name",
+    key: "name",
+  },
+  {
+    type: "string",
+    title: "value",
+    key: "value",
+  },
+  {
+    type: "image",
+    title: "image",
+    key: "image",
+  },
+]
+
+const Category = () => {
+  const {
+    isOpen: isModalTemplateOpen,
+    onOpen: onModalTemplateOpen,
+    onClose: onModalTemplateClose,
+  } = useDisclosure()
+
+  const {
+    isOpen: isDeleteConfirmOpen,
+    onOpen: onDeleteConfirmOpen,
+    onClose: onDeleteConfirmClose,
+  } = useDisclosure()
+
+  const [searchCondition, setSearchCondition] = useState<{
+    [key: string]: string | number
+  }>({ page: 1, limit: 10 })
+  const [tableData, setTableData] = useState<TableDataProps[]>([])
+  const [totalSize, setTotalSize] = useState(0)
+  const [editInfo, setEditInfo] = useState({})
+  const [deleteId, setDeleteId] = useState("")
+
+  const SearchFields: FieldProps[] = [
+    {
+      title: "Name",
+      value: "name",
+      type: "text",
+      placeholder: "Search by name",
+    },
+    {
+      title: "Value",
+      value: "value",
+      type: "text",
+      placeholder: "Search by value",
+    },
+  ]
+
+  useEffect(() => {
+    // clear edit info when close modal
+    if (!isModalTemplateOpen) setEditInfo({})
+  }, [isModalTemplateOpen])
+
+  useEffect(() => {
+    getCategoryList()
+  }, [searchCondition])
+
+  function getCategoryList(order = "desc") {
+    getCategory({ order, ...searchCondition })
+      .then(res => {
+        setTableData(res.data.categories)
+        setTotalSize(res.data.totalSize)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  function actionClick(type: "edit" | "delete", id: string) {
+    if (type === "edit") {
+      getCategory({ id }).then(res => {
+        setEditInfo(res.data.categories[0])
+        onModalTemplateOpen()
+      })
+    } else {
+      setDeleteId(id)
+      onDeleteConfirmOpen()
+    }
+  }
+
+  return (
+    <Box h="100%">
+      <Flex h="100%" direction="column">
+        <SearchForm
+          defaultValue={{
+            name: "",
+            value: "",
+          }}
+          fields={SearchFields}
+          onSubmit={val =>
+            setSearchCondition({ page: searchCondition.page, ...val })
+          }
+          onCreateClick={onModalTemplateOpen}
+          createTitle="Create Category"
+        />
+        <Table
+          tableHeader={TABLE_HEADER}
+          tableData={tableData}
+          onActionClick={actionClick}
+        />
+        <Pagination
+          page={searchCondition.page as number}
+          totalSize={totalSize}
+          onPageChange={changedPage => {
+            console.log(changedPage)
+            setSearchCondition({ ...searchCondition, page: changedPage + 1 })
+          }}
+        />
+      </Flex>
+      <ModalTemplate
+        isOpen={isModalTemplateOpen}
+        onClose={onModalTemplateClose}
+        refetchData={getCategoryList}
+        editData={editInfo}
+      />
+      <Modal
+        size="md"
+        title="Comfirm Delete?"
+        isOpen={isDeleteConfirmOpen}
+        onClose={() => {
+          setDeleteId("")
+          onDeleteConfirmClose()
+        }}
+        submitText="Comfirm"
+        onSubmit={e => {
+          e.preventDefault()
+          deleteCategory(deleteId)
+            .then(() => {
+              getCategoryList()
+            })
+            .finally(() => {
+              setDeleteId("")
+              onDeleteConfirmClose()
+            })
+        }}
+      >
+        <FiAlertCircle fontSize={64} color="#E53E3E" />
+        <Text fontSize={24}>Can't be recovered after deletion</Text>
+      </Modal>
+    </Box>
+  )
+}
+
+export default Category
---
+++ src/pages/Category/ModalTemplate.tsx
@@ -0,0 +1,168 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+import FileUpload from "../../components/FileUpload"
+import Modal from "../../components/Modal"
+import convertImageToBase64 from "../../utils/convertImageToBase64"
+import { postCategory, patchCategory } from "../../api"
+
+
+interface ModalTemplateProps {
+  isOpen: boolean
+  onClose: () => void
+  refetchData: () => void
+  editData: { [key: string]: string | number }
+}
+
+const ModalTemplate = ({
+  isOpen,
+  onClose,
+  refetchData,
+  editData = {},
+}: ModalTemplateProps) => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    resetField,
+    watch,
+    formState: { errors },
+    setValue,
+  } = useForm({})
+
+  const watchPreviewImg = watch("image", [])
+
+  const isPreviewImgObject = typeof watchPreviewImg === "object"
+  const isEdit = !!editData.id
+
+  const uploadImageUrl = () => {
+    if (isPreviewImgObject) {
+      if (watchPreviewImg) return watchPreviewImg[0]
+      return undefined
+    }
+    return watchPreviewImg
+  }
+
+  const validateFiles = (value: FileList) => {
+    if (value.length < 1) {
+      return "Files is required"
+    }
+    for (const file of Array.from(value)) {
+      const fsMb = file.size / (1024 * 1024)
+      const MAX_FILE_SIZE = 10
+      if (fsMb > MAX_FILE_SIZE) {
+        return "Max file size 10mb"
+      }
+    }
+    return true
+  }
+
+  useEffect(() => {
+    Object.entries(editData).forEach(([key, val]) => {
+      setValue(key, val)
+    })
+  }, [editData])
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {
+        onClose()
+        reset({})
+      }}
+      title={`${isEdit ? "Edit" : "Create"} Category`}
+      submitText={isEdit ? "Edit" : "Create"}
+      onSubmit={handleSubmit(async val => {
+        console.log(val)
+        if (isEdit) {
+          // edit
+          const base64Image = isPreviewImgObject
+            ? await convertImageToBase64(val.image[0]).then(data => data)
+            : val.image
+
+          patchCategory(editData.id, {
+            ...val,
+            image: base64Image,
+          }).then(() => refetchData())
+        } else {
+          // create
+          const base64Image = await convertImageToBase64(val.image[0]).then(
+            data => data
+          )
+
+          postCategory({
+            ...val,
+            image: base64Image,
+          }).then(() => refetchData())
+        }
+
+        onClose()
+        reset({})
+      })}
+    >
+      <FileUpload
+        errors={errors}
+        register={register("image", { validate: validateFiles })}
+        uploadFile={uploadImageUrl()}
+        handleClear={() => resetField("image")}
+      />
+      <FormField
+        errors={errors}
+        title="Name"
+        value="name"
+        placeholder="Input name"
+        detail={register("name", { required: "Please input name" })}
+      />
+      <FormField
+        errors={errors}
+        title="Value"
+        value="value"
+        placeholder="Input value"
+        detail={register("value", { required: "Please input value" })}
+      />
+    </Modal>
+  )
+}
+
+interface FormFieldProps {
+  errors: { [x: string]: any }
+  title: string
+  value: string
+  detail: any
+  placeholder: string
+}
+
+const FormField = ({
+  errors,
+  title,
+  value,
+  detail,
+  placeholder,
+}: FormFieldProps) => {
+  const borderColor = useColorModeValue("gray.700", "gray.200")
+
+  return (
+    <FormControl isRequired isInvalid={errors[value]}>
+      <FormLabel fontSize="14px" htmlFor={value}>
+        {title}
+      </FormLabel>
+      <Input
+        placeholder={placeholder}
+        borderColor={borderColor}
+        required={false}
+        {...detail}
+      />
+      <FormErrorMessage>
+        {errors[value] && errors[value].message}
+      </FormErrorMessage>
+    </FormControl>
+  )
+}
+
+export default ModalTemplate
