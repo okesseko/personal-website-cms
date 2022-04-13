c18c07afc850d897b81803174a0b690269cb61cf
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-14 00:15:35 +0800

    feat: set private router to hidden page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,57 @@
-import React from "react";
+import React from "react"
 
-import { Box, ChakraProvider, theme } from "@chakra-ui/react";
+import { Box, ChakraProvider, theme } from "@chakra-ui/react"
 
-import Header from "./components/Header";
-import SimpleSidebar from "./components/Siderbar";
-import { BrowserRouter, Route, Outlet, Routes, Navigate } from "react-router-dom";
-import Article from "./pages/article/Article";
-import Category from "./pages/Category/Category";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import Account from "./pages/Account";
+import Header from "./components/Header"
+import SimpleSidebar from "./components/Siderbar"
+import {
+  BrowserRouter,
+  Route,
+  Outlet,
+  Routes,
+  Navigate,
+  PathRouteProps,
+} from "react-router-dom"
+import Article from "./pages/article/Article"
+import Category from "./pages/Category/Category"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
+import Account from "./pages/Account"
+import { getAuthHeader } from "./api"
 
-export const App = () => (
-  <BrowserRouter>
-    <ChakraProvider theme={theme}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <Header />
-              <Box textAlign="center" fontSize="xl">
-                <SimpleSidebar>
-                  <Outlet />
-                </SimpleSidebar>
-              </Box>
-            </div>
-          }
-        >
-          <Route path="" element={<Navigate to="/article" replace />} />
-          <Route path="/article" element={<Article />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/account" element={<Account />} />
-        </Route>
+export const App = () => {
+  
+  const PrivateRootRouter = () => {
+    const isLogin = getAuthHeader()
+    return isLogin ? (
+      <div>
+        <Header />
+        <Box textAlign="center" fontSize="xl">
+          <SimpleSidebar>
+            <Outlet />
+          </SimpleSidebar>
+        </Box>
+      </div>
+    ) : (
+      <Navigate to="/login" replace />
+    )
+  }
 
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </ChakraProvider>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<PrivateRootRouter />}>
+            <Route path="" element={<Navigate to="/article" replace />} />
+            <Route path="/article" element={<Article />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
+  )
+}
