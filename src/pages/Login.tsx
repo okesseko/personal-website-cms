850b06f54a6e67f32fc9c658822260fa6a316879
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-13 22:47:05 +0800

    feat: add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import { useState } from "react"
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  Link,
+  Avatar,
+  FormControl,
+  FormHelperText,
+  InputRightElement,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import { FaUserAlt, FaLock } from "react-icons/fa"
+
+const CFaUserAlt = chakra(FaUserAlt)
+const CFaLock = chakra(FaLock)
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleShowClick = () => setShowPassword(!showPassword)
+
   return (
-    <div>Login</div>
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor={useColorModeValue("gray.200", "gray.900")}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar bg={useColorModeValue("teal.500", "teal.700")} />
+        <Heading color={useColorModeValue("teal.400", "teal.600")}>
+          Welcome
+        </Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor={useColorModeValue("whiteAlpha.900", "gray.700")}
+              boxShadow="md"
+              borderRadius="8px"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input placeholder="account" />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handleShowClick}
+                      color="black"
+                    >
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Box marginTop="48px !important">
+                <Button
+                  borderRadius={8}
+                  type="submit"
+                  variant="solid"
+                  colorScheme="teal"
+                  width="full"
+                >
+                  Login
+                </Button>
+              </Box>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
   )
 }
 
