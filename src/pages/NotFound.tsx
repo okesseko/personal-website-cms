cadc3979770234ec82abcaa15f7395d2ea4e7ce1
JImmy <59650459+okesseko@users.noreply.github.com>
2022-04-15 22:42:35 +0800

    feat: add page no found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Flex, Button, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <Flex
+      h="100vh"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+    >
+      <Text fontSize="72px">Oops!</Text>
+      <Text fontSize="50px" marginTop="24px" marginBottom="36px">
+        Seems you are missing
+      </Text>
+      <Button
+        height="fit-content"
+        fontSize="24px"
+        padding="16px 16px"
+        colorScheme="blue"
+      >
+        <Link to="/">Click me go back</Link>
+      </Button>
+    </Flex>
   )
 }
 
